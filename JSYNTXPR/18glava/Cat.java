e5f581723428e1ed97abc50bf2091f0ab01f4a4d
kol4egraf0-0 <genitya408@gmail.com>
2025-05-22 18:28:24 +0300

    add polimorfizm3

---
+++ JSYNTXPR/18glava/Cat.java
@@ -0,0 +1,15 @@
+public class Cat extends Astronaut{
+    public String getInfo(){
+        return "Cat";
+    }
+}
+/*
+ * public class Cat extends Pet {
+    public static final String CAT = "Я не люблю людей.";
+    @Override
+    public void printInfo(){
+      super.printInfo();
+      System.out.println(CAT);
+    }
+}
+ */
---
+++ JSYNTXPR/18glava/Dog.java
@@ -0,0 +1,15 @@
+public class Dog extends Astronaut{
+    public String getInfo(){
+        return "Dog";
+    }
+}
+/*
+ * public class Dog extends Pet {
+    public static final String DOG = "Я люблю людей.";
+    @Override
+    public void printInfo(){
+      super.printInfo();
+      System.out.println(DOG);
+    }
+}
+ */
