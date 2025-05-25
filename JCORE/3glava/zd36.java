9ee3638fb51f0c55827c752752fc35803903c82e
kol4egraf0-0 <genitya408@gmail.com>
2025-05-25 23:40:26 +0300

    add to glava s abstract methods

---
+++ JCORE/3glava/zd36.java
@@ -0,0 +1,54 @@
+public class zd36 {
+public static void main(String[] args) {
+
+    }
+
+    public interface Movable {
+        void move();
+    }
+
+    public interface Edible {
+        void beEaten();
+    }
+
+    public interface Eat {
+        void eat();
+    }
+
+    class Cat implements Movable, Edible, Eat {
+
+        public void eat() {
+
+        }
+
+        public void beEaten() {
+
+        }
+
+        public void move() {
+
+        }
+    }
+
+    class Mouse implements Movable, Edible {
+
+        public void beEaten() {
+
+        }
+
+        public void move() {
+
+        }
+    }
+
+    class Dog implements Movable, Eat {
+
+        public void eat() {
+
+        }
+
+        public void move() {
+
+        }
+    }
+}
